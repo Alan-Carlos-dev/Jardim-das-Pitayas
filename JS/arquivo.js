8aea28dda45864901885be6c003f655d66274dd8
ALAN CARLOS BERNARDES DE LIMA <129783362+Alan-Carlos-dev@users.noreply.github.com>
2026-04-10 18:38:25 -0300

    modificação das funcionalidades do botão do navbar

--- JS/arquivo.js
+++ JS/arquivo.js
@@ -1,14 +1,16 @@
 // script.js
+window.onload = function() {
+    alert("Bem-vindo(a) ao Jardim das Pitayas! \n\n⚠️ Este site encontra-se atualmente em fase de construção.");
+};
+
 const loginForm = document.getElementById('login-form');
 
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
 
-    // Aqui você pode adicionar a lógica de login
-    // Por exemplo, você pode fazer uma requisição AJAX para um servidor
-    // ou verificar se as credenciais estão corretas
-
-    console.log(`Usuário: ${username}, Senha: ${password}`);
-});
+        console.log(`Usuário: ${username}, Senha: ${password}`);
+    });
+}
